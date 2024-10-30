30d89955af48b1e366edd30a2d499aadaaca9b5b
tester2024 <mohamadhossin.rezaii@gmail.com>
2024-10-30 13:36:58 +0330

    update

--- gateway/gateway_windows.go
+++ gateway/gateway_windows.go
@@ -1,80 +1,32 @@
 //go:build windows
+// +build windows
 
 package gateway
 
 import (
-	"errors"
 	"net"
 	"os/exec"
-	"strings"
 	"syscall"
 )
 
-var (
-	ErrNoGateway = errors.New("no gateway found")
-	ErrCantParse = errors.New("unable to parse route output")
-)
-
-func DiscoverGatewayIPv4() (net.IP, error) {
-	return discoverGateway("0.0.0.0", parseIPv4RouteEntry)
-}
-
-func DiscoverGatewayIPv6() (net.IP, error) {
-	return discoverGateway("-6 ::/0", parseIPv6RouteEntry)
-}
-
-func discoverGateway(routeArg string, parseFunc func([]byte) (string, error)) (net.IP, error) {
-	cmd := exec.Command("route", "print", routeArg)
-	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
-
-	output, err := cmd.CombinedOutput()
+func discoverGatewayOSSpecificIPv4() (ip net.IP, err error) {
+	routeCmd := exec.Command("route", "print", "0.0.0.0")
+	routeCmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+	output, err := routeCmd.CombinedOutput()
 	if err != nil {
 		return nil, err
 	}
 
-	gatewayIPStr, err := parseFunc(output)
+	return parseWindowsGatewayIPv4(output)
+}
+
+func discoverGatewayOSSpecificIPv6() (ip net.IP, err error) {
+	routeCmd := exec.Command("route", "print", "-6", "::/0")
+	routeCmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+	output, err := routeCmd.CombinedOutput()
 	if err != nil {
 		return nil, err
 	}
 
-	gatewayIP := net.ParseIP(gatewayIPStr)
-	if gatewayIP == nil {
-		return nil, ErrCantParse
-	}
-
-	return gatewayIP, nil
-}
-
-func parseIPv4RouteEntry(output []byte) (string, error) {
-	lines := strings.Split(string(output), "\n")
-	for i, line := range lines {
-		if strings.Contains(line, "Active Routes") {
-			if len(lines) <= i+2 {
-				return "", ErrNoGateway
-			}
-			fields := strings.Fields(lines[i+2])
-			if len(fields) < 5 {
-				return "", ErrCantParse
-			}
-			return fields[2], nil
-		}
-	}
-	return "", ErrNoGateway
-}
-
-func parseIPv6RouteEntry(output []byte) (string, error) {
-	lines := strings.Split(string(output), "\n")
-	for i, line := range lines {
-		if strings.Contains(line, "Active Routes") {
-			if len(lines) <= i+2 {
-				return "", ErrNoGateway
-			}
-			fields := strings.Fields(lines[i+2])
-			if len(fields) < 4 {
-				return "", ErrCantParse
-			}
-			return fields[3], nil
-		}
-	}
-	return "", ErrNoGateway
+	return parseWindowsGatewayIPv6(output)
 }
