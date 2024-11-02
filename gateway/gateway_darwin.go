d4a3cef19c4b50100ec84c363f9ec2095d99e4d7
tester2024 <mohamadhossin.rezaii@gmail.com>
2024-11-02 10:43:05 +0330

    fixes

--- gateway/gateway_darwin.go
+++ gateway/gateway_darwin.go
@@ -6,30 +6,35 @@ import (
 	"errors"
 	"net"
 	"os/exec"
+	"strings"
 )
 
 var errCantParse = errors.New("unable to parse route output")
 
-func DiscoverGatewayIPv4() (ip net.IP, err error) {
-	ipstr, err := exec.Command("sh", "-c", "route -n get default | grep 'gateway' | awk 'NR==1{print $2}'").CombinedOutput()
+func DiscoverGatewayIPv4() (net.IP, error) {
+	cmd := exec.Command("sh", "-c", "route -n get default | grep 'gateway' | awk '{print $2}'")
+	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return nil, err
 	}
 
-	ipv4 := net.ParseIP(string(ipstr))
+	ipStr := strings.TrimSpace(string(output))
+	ipv4 := net.ParseIP(ipStr)
 	if ipv4 == nil {
 		return nil, errCantParse
 	}
 	return ipv4, nil
 }
 
-func DiscoverGatewayIPv6() (ip net.IP, err error) {
-	ipstr, err := exec.Command("sh", "-c", "route -6 -n get default | grep 'gateway' | awk 'NR==1{print $2}'").CombinedOutput()
+func DiscoverGatewayIPv6() (net.IP, error) {
+	cmd := exec.Command("sh", "-c", "route -6 -n get default | grep 'gateway' | awk '{print $2}'")
+	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return nil, err
 	}
 
-	ipv6 := net.ParseIP(string(ipstr))
+	ipStr := strings.TrimSpace(string(output))
+	ipv6 := net.ParseIP(ipStr)
 	if ipv6 == nil {
 		return nil, errCantParse
 	}
--- gateway/gateway_linux.go
+++ gateway/gateway_linux.go
@@ -6,28 +6,35 @@ import (
 	"errors"
 	"net"
 	"os/exec"
+	"strings"
 )
 
 var errCantParse = errors.New("unable to parse route output")
 
-func DiscoverGatewayIPv4() (ip net.IP, err error) {
-	ipstr, err := exec.Command("sh", "-c", "route -n | grep 'UG[ \t]' | awk 'NR==1{print $2}'").CombinedOutput()
+func DiscoverGatewayIPv4() (net.IP, error) {
+	cmd := exec.Command("sh", "-c", "ip route | grep 'default' | awk '{print $3}'")
+	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return nil, err
 	}
-	ipv4 := net.ParseIP(string(ipstr))
+
+	ipStr := strings.TrimSpace(string(output))
+	ipv4 := net.ParseIP(ipStr)
 	if ipv4 == nil {
 		return nil, errCantParse
 	}
 	return ipv4, nil
 }
 
-func DiscoverGatewayIPv6() (ip net.IP, err error) {
-	ipstr, err := exec.Command("sh", "-c", "route -6 -n | grep 'UG[ \t]' | awk 'NR==1{print $2}'").CombinedOutput()
+func DiscoverGatewayIPv6() (net.IP, error) {
+	cmd := exec.Command("sh", "-c", "ip -6 route | grep 'default' | awk '{print $3}'")
+	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return nil, err
 	}
-	ipv6 := net.ParseIP(string(ipstr))
+
+	ipStr := strings.TrimSpace(string(output))
+	ipv6 := net.ParseIP(ipStr)
 	if ipv6 == nil {
 		return nil, errCantParse
 	}
