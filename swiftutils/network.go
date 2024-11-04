e363439abd7618c3901824aa6a45afc16efe1062
tester2024 <mohamadhossin.rezaii@gmail.com>
2024-11-05 01:03:58 +0330

    update

---
+++ swiftutils/network.go
@@ -0,0 +1,15 @@
+package swiftutils
+
+import "net"
+
+func IsIPv4(packet []byte) bool {
+	return 4 == (packet[0] >> 4)
+}
+
+func IsIPv6(packet []byte) bool {
+	return 6 == (packet[0] >> 4)
+}
+
+func IPv4Destination(packet []byte) net.IP {
+	return net.IPv4(packet[16], packet[17], packet[18], packet[19])
+}
