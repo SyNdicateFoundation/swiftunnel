2b9c1c660e3c7031d98574d973673d65f16c5742
tester2024 <mohamadhossin.rezaii@gmail.com>
2026-01-06 19:16:09 +0330

    update

--- manage_windows.go
+++ manage_windows.go
@@ -69,22 +69,22 @@ func (a *SwiftInterface) SetMTU(mtu int) error {
 	if err != nil {
 		return err
 	}
-
+	
 	var ifRow windows.MibIfRow
 	ifRow.Index = uint32(adapterIndex)
-
+	
 	ret, _, _ := procGetIfEntry.Call(uintptr(unsafe.Pointer(&ifRow)))
 	if err := windows.Errno(ret); !errors.Is(err, windows.ERROR_SUCCESS) {
 		return fmt.Errorf("failed to retrieve interface entry: %w", err)
 	}
-
+	
 	ifRow.Mtu = uint32(mtu)
-
+	
 	ret, _, _ = procSetIfEntry.Call(uintptr(unsafe.Pointer(&ifRow)))
 	if err := windows.Errno(ret); !errors.Is(err, windows.ERROR_SUCCESS) {
 		return fmt.Errorf("failed to set MTU: %w", err)
 	}
-
+	
 	return nil
 }
 
@@ -94,11 +94,11 @@ func (a *SwiftInterface) SetUnicastIpAddressEntry(config *swiftypes.UnicastConfi
 	if err != nil {
 		return err
 	}
-
+	
 	var addressRow windows.MibUnicastIpAddressRow
-
+	
 	_, _, _ = procInitializeUnicastIpAddressEntry.Call(uintptr(unsafe.Pointer(&addressRow)))
-
+	
 	if ipv4 := config.IP.To4(); ipv4 != nil {
 		addressRow.Address.Family = syscall.AF_INET
 		copy(addressRow.Address.Addr[:net.IPv4len], ipv4)
@@ -108,21 +108,25 @@ func (a *SwiftInterface) SetUnicastIpAddressEntry(config *swiftypes.UnicastConfi
 	} else {
 		return fmt.Errorf("invalid IP address: %s", config.IP)
 	}
-
+	
 	ones, bits := config.IPNet.Mask.Size()
 	if ones > bits {
 		return fmt.Errorf("invalid subnet mask: %v", config.IPNet.Mask)
 	}
-
+	
+	if config.DadState == windows.IpDadStateInvalid {
+		addressRow.DadState = windows.IpDadStatePreferred
+	}
+	
 	addressRow.InterfaceLuid = luid.ToUint64()
 	addressRow.OnLinkPrefixLength = uint8(ones)
 	addressRow.DadState = config.DadState
-
+	
 	ret, _, _ := procCreateUnicastIpAddressEntry.Call(uintptr(unsafe.Pointer(&addressRow)))
 	if errno := windows.Errno(ret); !errors.Is(errno, windows.ERROR_SUCCESS) && !errors.Is(errno, windows.ERROR_OBJECT_ALREADY_EXISTS) {
 		return fmt.Errorf("failed to create unicast IP address config: %w (error code: %d)", errno, ret)
 	}
-
+	
 	return nil
 }
 
@@ -132,10 +136,10 @@ func (a *SwiftInterface) SetDNS(config *swiftypes.DNSConfig) error {
 	if err != nil {
 		return err
 	}
-
+	
 	var settings dnsInterfaceSettings
 	settings.Version = 1
-
+	
 	ret, _, _ := procGetInterfaceDnsSettings.Call(
 		uintptr(unsafe.Pointer(&guid)),
 		uintptr(unsafe.Pointer(&settings)),
@@ -143,7 +147,7 @@ func (a *SwiftInterface) SetDNS(config *swiftypes.DNSConfig) error {
 	if err := windows.Errno(ret); !errors.Is(err, windows.ERROR_SUCCESS) {
 		return fmt.Errorf("failed to get DNS settings: %w", windows.Errno(ret))
 	}
-
+	
 	if config.Domain != "" {
 		domain, err := windows.UTF16PtrFromString(config.Domain)
 		if err != nil {
@@ -152,31 +156,31 @@ func (a *SwiftInterface) SetDNS(config *swiftypes.DNSConfig) error {
 		settings.Domain = domain
 		settings.Flags |= dnsSettingDomain
 	}
-
+	
 	if len(config.DnsServers) > 0 {
 		var servers []string
 		var ipv6 bool
-
+		
 		for _, server := range config.DnsServers {
 			if server.To4() == nil {
 				ipv6 = true
 			}
 			servers = append(servers, server.String())
 		}
-
+		
 		nameServer, err := windows.UTF16PtrFromString(strings.Join(servers, ","))
 		if err != nil {
 			return fmt.Errorf("failed to convert DNS servers to UTF16: %w", err)
 		}
-
+		
 		settings.NameServer = nameServer
 		settings.Flags |= dnsSettingNameserver
-
+		
 		if ipv6 {
 			settings.Flags |= dnsSettingIpv6
 		}
 	}
-
+	
 	ret, _, _ = procSetInterfaceDnsSettings.Call(
 		uintptr(unsafe.Pointer(&guid)),
 		uintptr(unsafe.Pointer(&settings)),
@@ -184,7 +188,7 @@ func (a *SwiftInterface) SetDNS(config *swiftypes.DNSConfig) error {
 	if err := windows.Errno(ret); !errors.Is(err, windows.ERROR_SUCCESS) {
 		return fmt.Errorf("failed to set DNS settings: %w", windows.Errno(ret))
 	}
-
+	
 	return nil
 }
 
@@ -194,14 +198,14 @@ func (a *SwiftInterface) AddRoute(route netlink.Route) error {
 	if err != nil {
 		return err
 	}
-
+	
 	var row windows.MibIpForwardRow2
-
+	
 	ret, _, _ := procInitializeIpForwardEntry.Call(uintptr(unsafe.Pointer(&row)))
 	if err := windows.Errno(ret); !errors.Is(err, windows.ERROR_SUCCESS) {
 		return err
 	}
-
+	
 	row.InterfaceLuid = luid.ToUint64()
 	row.InterfaceIndex = 0
 	row.Metric = uint32(route.Priority)
@@ -209,11 +213,11 @@ func (a *SwiftInterface) AddRoute(route netlink.Route) error {
 	row.Origin = nlRouteOriginManual
 	row.ValidLifetime = 0xffffffff
 	row.PreferredLifetime = 0xffffffff
-
+	
 	if route.Dst != nil {
 		ones, _ := route.Dst.Mask.Size()
 		row.DestinationPrefix.PrefixLength = uint8(ones)
-
+		
 		if ipv4 := route.Dst.IP.To4(); ipv4 != nil {
 			row.DestinationPrefix.Prefix.Family = syscall.AF_INET
 			copy((*[4]byte)(unsafe.Pointer(&row.DestinationPrefix.Prefix.Data[0]))[:], ipv4)
@@ -234,7 +238,7 @@ func (a *SwiftInterface) AddRoute(route netlink.Route) error {
 			return errors.New("cannot determine address family for default route (dst=nil, gw=nil)")
 		}
 	}
-
+	
 	if route.Gw != nil {
 		if ipv4 := route.Gw.To4(); ipv4 != nil {
 			row.NextHop.Family = syscall.AF_INET
@@ -244,7 +248,7 @@ func (a *SwiftInterface) AddRoute(route netlink.Route) error {
 			copy((*[16]byte)(unsafe.Pointer(&row.NextHop.Data[1]))[:], ipv6)
 		}
 	}
-
+	
 	ret, _, _ = procCreateIpForwardEntry2.Call(uintptr(unsafe.Pointer(&row)))
 	if errno := windows.Errno(ret); !errors.Is(errno, windows.ERROR_SUCCESS) {
 		if errors.Is(errno, windows.ERROR_OBJECT_ALREADY_EXISTS) {
@@ -252,7 +256,7 @@ func (a *SwiftInterface) AddRoute(route netlink.Route) error {
 		}
 		return fmt.Errorf("failed to add route: %w", errno)
 	}
-
+	
 	return nil
 }
 
@@ -262,26 +266,26 @@ func (a *SwiftInterface) SetStatus(status swiftypes.InterfaceStatus) error {
 	if err != nil {
 		return err
 	}
-
+	
 	var ifRow windows.MibIfRow
 	ifRow.Index = uint32(index)
-
+	
 	ret, _, _ := procGetIfEntry.Call(uintptr(unsafe.Pointer(&ifRow)))
 	if err := windows.Errno(ret); !errors.Is(err, windows.ERROR_SUCCESS) {
 		return fmt.Errorf("failed to retrieve interface entry: %w", err)
 	}
-
+	
 	switch status {
 	case swiftypes.InterfaceUp:
 		ifRow.AdminStatus = windows.IfOperStatusUp
 	case swiftypes.InterfaceDown:
 		ifRow.AdminStatus = windows.IfOperStatusDown
 	}
-
+	
 	ret, _, _ = procSetIfEntry.Call(uintptr(unsafe.Pointer(&ifRow)))
 	if err := windows.Errno(ret); !errors.Is(err, windows.ERROR_SUCCESS) {
 		return fmt.Errorf("failed to set interface status: %w", err)
 	}
-
+	
 	return nil
 }
